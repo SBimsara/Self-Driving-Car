6feb989ed3e5951d2844738ed28331c03d42d2b7
Sithira Bimsara <113254019+SBimsara@users.noreply.github.com>
2024-02-27 10:15:15 +0530

    Initial commit

---
+++ sensor.js
@@ -0,0 +1,22 @@
+class Sensor {
+    constructor(car){
+        this.car = car;
+        this.rayCount = 3;
+        this.rayLenght = 100;
+        this.raySpread = Math.PI/4;
+
+        this.rays = [];
+    }
+
+    update() {
+        this.rays = [];
+
+        for(let i=0; i<this.rayCount; i++){
+            const rayAngle = lerp(
+                this.raySpread/2,
+                -this.raySpread/2,
+                i/(this.rayCount-1)
+            );
+        }
+    }
+}
